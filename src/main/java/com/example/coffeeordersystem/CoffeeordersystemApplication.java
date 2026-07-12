330fbc14096d1611bdd96b253fccf99334c99fa8
WookJaes <dnrwo09@gmail.com>
2026-07-12 23:23:19 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/example/coffeeordersystem/CoffeeordersystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeordersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeordersystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coffeeordersystem/CoffeeordersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeeordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeordersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
